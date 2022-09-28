5d058d8cb99f9849dcddb4792bd6a7cd3c9a60c9
g.prassas <4only4games@gmail.com>
2022-09-28 20:00:38 +0300

    create navbar component

---
+++ src/app/components/shared-components/navbar/navbar.component.ts
@@ -0,0 +1,35 @@
+
+import { Component, OnInit } from '@angular/core';
+import { StoreService } from 'src/services/store.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  stores:any;
+  constructor( private storeService:StoreService) { }
+
+
+  ngOnInit(): void {
+  }
+
+
+//   searchStoreHandler(searchString:string){
+//     this.storeService.search(searchString).subscribe({
+//         next: (response: any) => this.stores = response,
+//         error: (error: any) => console.log(error),
+//         complete: () => console.log("petuxe")
+//   })
+// }
+searchStoresByCategory(){
+  this.storeService.getStoreCategories().subscribe({
+    next: (response: any) => this.stores = response,
+    error: (error: any) => console.log(error),
+    complete: () => console.log("petuxe")
+})
+}
+
+}
