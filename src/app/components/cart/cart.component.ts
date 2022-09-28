c3fa9f153c5fcfcd45995f9b1c42b8bf8e5090cd
g.prassas <4only4games@gmail.com>
2022-09-28 20:01:44 +0300

    create cart component

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  FinallizeOrder(){
+    alert("Η Παραγγελία σου ολοκληρώθηκε! Στην πόρτα σου σε 25 λεπτά ...")
+  }
+
+}
