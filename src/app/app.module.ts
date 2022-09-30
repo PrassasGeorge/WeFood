7c0dc54743f442697a22ac8b446551f9572ee5ef
g.prassas <4only4games@gmail.com>
2022-09-30 22:40:42 +0300

    make small adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared-components/navbar/navbar.component';
+import { FooterComponent } from './components/shared-components/footer/footer.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { StoreComponent } from './components/store/store.component';
+import { StoreCategoryComponent } from './components/store-category/store-category.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreItemComponent } from './components/store-item/store-item.component';
+import { UserComponent } from './components/user/user.component';
+import { OrderComponent } from './components/order/order.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { PopularStoresComponent } from './components/popular-stores/popular-stores.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CartComponent,
+    ContactUsComponent,
+    StoreComponent,
+    StoreCategoryComponent,
+    HomeComponent,
+    StoreItemComponent,
+    UserComponent,
+    OrderComponent,
+    RegisterComponent,
+    LogInComponent,
+    PopularStoresComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
