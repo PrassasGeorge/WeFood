236c87a87812d91a6f7829cfc44b92e9a9af55fa
g.prassas <4only4games@gmail.com>
2022-09-30 22:40:14 +0300

    update routing imports and paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { StoreItemComponent } from './components/store-item/store-item.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UserComponent } from './components/user/user.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'Contact-us', component:ContactUsComponent},
+  {path:'Cart' , component:CartComponent},
+  {path:'User' , component:UserComponent},
+  {path:'Register' , component:RegisterComponent},
+  {path:'Log-in' , component:LogInComponent},
+  {path:'Store-item' , component:StoreItemComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
