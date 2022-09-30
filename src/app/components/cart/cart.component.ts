90c8b0b74d8e740521c2454a015744ce5a0a5699
Giorgos <giorgosthf@gmail.com>
2022-10-01 02:43:46 +0300

    Implement cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {CartService} from "../../../services/cart.service";
 
 @Component({
   selector: 'app-cart',
@@ -7,10 +9,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cartService:CartService,private route:ActivatedRoute) { }
 
-  ngOnInit(): void {
+  item:any;
+  quantity: any;
+  newOrder:any;
+  finalOrder:any;
+  paymentMethod:any;
+
+  ngOnInit(): void {}
+
+
+  order = JSON.parse(this.cartService.getOrder());
+
+
+
+  updateItem(item:string,quantity: number){
+    this.cartService.updateItem(item, quantity).subscribe(
+      {
+        next: (response: any) => {
+          this.newOrder = response;
+          localStorage.setItem('newOrder', JSON.stringify(this.newOrder));
+          this.order = this.newOrder;
+
+        },
+        error: (error: any) => console.error(error),
+        complete: () => console.log(this.newOrder)
+
+      }
+    )
+
   }
+
+  deleteItem(item:string){
+    this.cartService.deleteItem(item).subscribe(
+      {
+        next:(response: any) => {
+          this.newOrder = response;
+          localStorage.setItem('newOrder', JSON.stringify(this.newOrder));
+          this.order=this.newOrder;
+
+        },
+
+        error: (error: any) => console.error(error),
+        complete: () => console.log(this.newOrder)
+
+      }
+    )
+  }
+
+  checkout(paymentMethod:string){
+
+    this.cartService.checkout(paymentMethod).subscribe(
+      {
+        next:(response:any) => {
+          this.finalOrder = response;
+
+
+        },
+        error:(error:any)=> console.log(error),
+        complete: ()=> {
+          console.log("Order Created");
+
+        }
+      }
+    )
+  }
+
+  clearCart(){
+    localStorage.removeItem('newOrder');
+    this.order = null;
+  }
+
 
   FinallizeOrder(){
     alert("Η Παραγγελία σου ολοκληρώθηκε! Στην πόρτα σου σε 25 λεπτά ...")
